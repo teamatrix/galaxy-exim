e73fa82eb9f1cc53ffccf7cdd8d2c2425ada70c2
kevinklev <kevinkm2009@gmail.com>
2022-11-13 17:43:47 -0500

    Add time label feature

--- files/main.py
+++ files/main.py
@@ -4,6 +4,7 @@ import time
 
 
 now = time.strftime("%b %d, %Y %H: %M :%S")
+print("The time is below: ")
 print("It is", now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
