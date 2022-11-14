8327dfa9dd407636b23833253139f85880471394
kevinklev <kevinkm2009@gmail.com>
2022-11-14 18:25:37 -0500

    Implemented the add button

--- files/gui.py
+++ files/gui.py
@@ -2,15 +2,28 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg. InputText(tooltip="Enter todo")
+input_box = sg. InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do App", layout=[[label, input_box, add_button]])
+window = sg.Window('My To-Do App',
+                   layout=[[label, input_box, add_button]],
+                   font=('Helvetica', 20))
 
 """ Commented code below will display the input screen on a command line below the label"""
 
 # window = sg.Window("My To-Do App", layout=[[label], [input_box]])
 
 
-window.read()
+while True:
+    event, value = window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
