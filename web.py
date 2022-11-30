417447d0b7adeafd2eb8bc790f734ef869052089
kevinklev <kevinkm2009@gmail.com>
2022-11-30 18:46:38 -0500

    Implement add new to do item features

--- web.py
+++ web.py
@@ -14,8 +14,13 @@ st.title("My Todo App")
 st.subheader("This is My todo app")
 st.write("Developed by TeaMatriX.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
